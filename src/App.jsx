2c04017a388e570ce284085b4a4c779e6cf43a17
its-bismay <2023ugee070@nitjsr.ac.in>
2024-10-12 19:22:25 +0530

    routes and theme setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,60 @@
 import React from 'react'
-import { Button } from './components/ui/button'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import AppLayout from './layouts/AppLayout';
+import LandingPage from './pages/LandingPage';
+import Onboarding from './pages/Onboarding';
+import JobListing from './pages/JobListing';
+import Job from './pages/Job';
+import PostJobs from './pages/PostJobs';
+import SavedJobs from './pages/SavedJobs';
+import MyJobs from './pages/MyJobs';
+import { ThemeProvider } from './components/ThemeProvider';
+
+const router = createBrowserRouter([
+  {
+    element:<AppLayout></AppLayout>,
+    children:[
+      {
+        path:'/',
+        element:<LandingPage></LandingPage>
+      },
+      {
+        path:'/onboarding',
+        element:<Onboarding />
+      },
+      {
+        path:'/joblisting',
+        element:<JobListing />
+      },
+      {
+        path:'/jobs',
+        element:<JobListing />
+      },
+      {
+        path:'/job/:id',
+        element:<Job />
+      },
+      {
+        path:'/post-job',
+        element:<PostJobs />
+      },
+      {
+        path:'/saved-jobs',
+        element:<SavedJobs />
+      },
+      {
+        path:'/my-jobs',
+        element:<MyJobs />
+      }
+    ]
+  }
+])
 
 const App = () => {
   return (
-    <div className='bg-black w-full h-screen text-2xl'>
-      <Button variant="destructive">Login</Button>
-    </div>
-  )
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    {<RouterProvider router={router}></RouterProvider>}
+  </ThemeProvider>);
 }
 
 export default App
